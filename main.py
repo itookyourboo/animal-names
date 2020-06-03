7ccadec02c2abf6c6fd9c61a8eb7616e62e524c0
itookyourboo <Wignorbo@yandex.ru>
2020-06-03 12:23:04 +0300

    Showing cat and dog names

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from base_skill.skill import *
 from .state import State
 from .strings import *
+from .dbhelper import get_name
 
 
 handler = CommandHandler()
@@ -16,37 +17,49 @@ def hello(req, res, session):
 @handler.command(words=tkn(WORDS_CAT), states=State.MAIN_MENU)
 def cat(res, req, session):
     res.text = txt(TEXT_CAT)
+    res.buttons = [button(x) for x in btn(BUTTONS_SEX)]
+    session['animal'] = 'cat'
+    session['state'] = State.SEX
 
 
 @handler.command(words=tkn(WORDS_DOG), states=State.MAIN_MENU)
 def dog(res, req, session):
     res.text = txt(TEXT_DOG)
     res.buttons = [button(x) for x in btn(BUTTONS_SEX)]
-    session['state'] = State.DOG_SEX
+    session['animal'] = 'dog'
+    session['state'] = State.SEX
 
 
-@handler.command(words=tkn(WORDS_DOG_BOY), states=State.DOG_SEX)
-def dog_boy(res, req, session):
-    res.text = txt(TEXT_DOG_NAME_FIRST) + txt(TEXT_DOG_NAME)
-    session['dog_boy'] = True
-    session['state'] = State.DOG_NAME
+@handler.command(words=tkn(WORDS_DOG_BOY), states=State.SEX)
+def boy(res, req, session):
+    session['sex'] = 'boy'
+    show_name(res, session, True)
 
 
-@handler.command(words=tkn(WORDS_DOG_GIRL), states=State.DOG_SEX)
-def dog_girl(res, req, session):
-    res.text = txt(TEXT_DOG_NAME_FIRST) + txt(TEXT_DOG_NAME)
-    session['dog_boy'] = False
-    session['state'] = State.DOG_NAME
+@handler.command(words=tkn(WORDS_DOG_GIRL), states=State.SEX)
+def girl(res, req, session):
+    session['sex'] = 'girl'
+    show_name(res, session, True)
 
 
-@handler.command(words=tkn(WORDS_MORE), states=State.DOG_NAME)
-def dog_more(res, req, session):
-    res.text = txt(TEXT_DOG_NAME)
+@handler.command(words=tkn(WORDS_MORE), states=State.NAME)
+def more(res, req, session):
+    show_name(res, session, False)
 
 
 @handler.undefined_command(states=tuple(State))
 def wtf(req, res, session):
     res.text = txt(TEXT_WTF)
+
+
+def show_name(res, session, intro=False):
+    session['state'] = State.NAME
+    x = txt(TEXT_NAME[session['animal']]).format(get_name(session['animal'], session['sex']))
+    if intro:
+        res.text = txt(TEXT_NAME_INTRO) + x
+    else:
+        res.text = x
+    res.buttons = [button(x) for x in btn(BUTTONS_NAME)]
 
 
 class AnimalNamesSkill(BaseSkill):
