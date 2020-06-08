2f88c6934339fc05200d62a8c6e5a26849053372
itookyourboo <Wignorbo@yandex.ru>
2020-06-08 08:56:24 +0300

    put_like, get_likes

--- dbhelper.py
+++ dbhelper.py
@@ -9,6 +9,35 @@ db = sqlite3.connect(DB_PATH)
 cursor = db.cursor()
 
 
+cursor.execute('create table if not exists like ('
+               'id integer primary key autoincrement, '
+               'user_id string, '
+               'animal string(10), '
+               'name string(50))')
+
+
 def get_name(animal, sex=None):
     sex_cond = f"where sex = '{sex}'" if sex else ''
     return cursor.execute(f"select name, sex from {animal} {sex_cond} order by random() limit 1").fetchone()[0]
+
+
+def add_wtf(res, req, session):
+    with open(os.path.join(BASE_DIR, 'wtf.txt'), 'a', encoding='utf8') as file:
+        file.write(f'{session["state"]}: {req.text}\n')
+
+
+def put_like(user_id, animal, name):
+    cursor.execute('insert into like(user_id, animal, name) values(?, ?, ?)',
+                   (user_id, animal, name))
+    db.commit()
+
+
+def get_likes(user_id):
+    return cursor.execute(f"select name, animal from like where user_id = '{user_id}'").fetchall()
+
+
+if __name__ == '__main__':
+    while True:
+        cursor.execute(input())
+        [print(i) for i in cursor.fetchall()]
+        db.commit()
